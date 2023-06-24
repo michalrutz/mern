b06119c8a188fbab44d4e5b0aa71ff8acb5c186e
Michal Rutz <michal.rutz@gmail.com>
2023-06-24 15:28:48 +0200

    first fetch

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from "express";
+import cors from "cors";
 const app = express();
+
+app.use(cors());
+
+app.get( "/", (req, res) => {
+        console.log("server is running");
+        res.json({ message:"OK" })
+})
 
 app.listen(3000, () => {
         console.log("Server is running on port 3000");
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [ data, setData ] = useState()
+
+  useEffect( () => {
+    (async () => {
+            try {
+              const response = await fetch( 'http://localhost:3000/' );
+              console.log(response)
+              const jsonData = await response.json();
+              setData(jsonData);
+            } catch (error) {
+              console.log('Error:', error);
+            }
+    })();
+  }, []);
 
   return (
     <>
       <div>
-        APP
-      </div>
-    
+        {  data ? data.message : "" }
+      </div>    
     </>
   )
 }
